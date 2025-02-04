d939883b58d56bd8c2ef4b2d4f20c34966f749d2
ehtishamnazir1234@gmail.com <ehtishamnazir1234@mail.com>
2025-02-04 16:18:15 +0500

    Updated feature or fixed a bug

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -8,6 +8,9 @@ import img6 from "../assets/namaz-time-icon-6.png";
 import "./Body.css";
 import img7 from "../assets/heading-img.png";
 import img8 from "../assets/text-circle.png";
+import img9 from "../assets/circle-img.png";
+import img10 from "../assets/real-history-book.jpg";
+import img11 from "../assets/real-history-boy.jpg";
 
 const Body = () => {
   const [prayerTImes, setPrayerTimes] = useState([]);
@@ -42,15 +45,66 @@ const Body = () => {
             ))}
           </div>
         </div>
-        <div className="heading">
-          <img src={img7} />
-          <p> Welcome to the Islamic Center</p>
-          <h2>Know The Real History of Islam</h2>
-        </div>
-        <div className="circle-img">
-          <img className='rotate' src={img8} alt="" />
-         
-        </div>  
+        <section class="position-relative">
+          <div class="container1">
+            <div class="heading">
+              <img src={img7} alt="icon" />
+              <p>Welcome to the Islamic Center</p>
+              <h2>Know The Real History of Islam</h2>
+            </div>
+            <div class="row">
+              <div class="col-lg-5">
+                <div class="islamic-history">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscsed do
+                    eiusmod tempor incididunt ut labore etedum dolor sit a ad
+                    minim veniam, quis nostr incididunt ut laborcitationn tempor
+                    incididunt ut labore um dolor sit amet, consectetur
+                    adipiscsed do eiu agna a liqua. Ut enim ad minim veniam,
+                    quis nostr incididunt ut laborcitation.
+                  </p>
+                  <ul class="list">
+                    <li>Astonishing Facilities</li>
+                    <li>Helping CommunitiesLeads </li>
+                    <li>Charity EventsSchooling Children</li>
+                    <li>Feeding Hungry People</li>
+                  </ul>
+                  <a href="#" class="btn">
+                    Read Our History
+                  </a>
+                </div>
+              </div>
+              <div class="col-lg-7">
+                <div class="row align-items-end">
+                  <div class="col-lg-5 col-6">
+                    <div class="circle-text">
+                      <img
+                        src={img8}
+                        class="circle-text-img"
+                        alt="circle-img"
+                      />
+                      <figure class="circle-img">
+                        <img src={img9} alt="circle-img" />
+                      </figure>
+                    </div>
+                    <div class="real-history-book hoverimg">
+                      <figure>
+                        <img src={img10} alt="img" />
+                      </figure>
+                    </div>
+                  </div>
+                  <div class="col-lg-7 col-6">
+                    <div class="real-history-book two hoverimg">
+                      <figure>
+                        <img src={img11} alt="real-history-boy" />
+                      </figure>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </section>
     </>
   );
